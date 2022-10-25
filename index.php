9d4f89b9d0736cacc444f50af437fc7d8ec470c3
Youssef <87530308+ben-houssine@users.noreply.github.com>
2022-10-25 22:11:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+<div>
+    <a href="index.php?page=start">Start</a>
+    <a href="index.php?page=contact">Contact</a>
+    <a href="index.php?page=legal">Legal</a>
+    <br></br>
+    
+    
+    <i>simple text-output which tells us on which site we currently are</i>
+  
+
+    
+    <?php
+        $haedline = '';
+        echo '<h1>' . $haedline  . '</h1>';
+       
+        if($_GET['page'] == 'contacts'){
+            echo "You are now on the contacts site";
+        }else {
+            echo "// You are currently on the: <b>". $_GET['page']." </b>- site";
+        }
+
+    ?>
+
+
+    </div>
+</body>
+
+</html>
